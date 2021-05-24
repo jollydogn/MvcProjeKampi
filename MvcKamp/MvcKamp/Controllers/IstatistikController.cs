d83b027456c7ce0fabb0ea577f0c7121e05b8fa7
Mustafa DOĞAN <77547784+jollydogn@users.noreply.github.com>
2021-05-24 13:00:16 +0300

    Update IstatistikController.cs

--- MvcKamp/MvcKamp/Controllers/IstatistikController.cs
+++ MvcKamp/MvcKamp/Controllers/IstatistikController.cs
@@ -24,7 +24,7 @@ namespace MvcKamp.Controllers
             var maxBaslikliKategori = _context.headings.Max(x => x.Category.CategoryName); 
             ViewBag.mission4 = maxBaslikliKategori;
 
-            var durumFark = (_context.Categories.Count(x => x.CategoryStatus == true))- (_context.Categories.Count(x => x.CategoryStatus == false)); // Kategoriler tablosundaki aktif kategori sayisi
+            var durumFark = (_context.Categories.Count(x => x.CategoryStatus == true))- (_context.Categories.Count(x => x.CategoryStatus == false));
             ViewBag.mission5 = durumFark;
             return View();
         }
